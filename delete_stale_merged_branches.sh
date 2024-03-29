586d8a37e44bb1afd992204e1c802afc23b84724
Jet Fontanilla <jetlogs@gmail.com>
2024-03-29 13:53:01 +0900

    script to delete stale merged branches

---
+++ delete_stale_merged_branches.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function init() {
+    git fetch --prune "origin"
+    ALL_BRANCH_LIST="$(git branch -r | grep -v "origin/master" | grep -v "origin/hotfix" | grep -v "origin/release")"
+    ALL_BRANCH_ARRAY=($ALL_BRANCH_LIST)
+    declare -a DELETE_BRANCH_LIST=()
+}
+
+error_exit()
+{
+    echo $1
+    exit 1
+}
+
+function delete_branch() {
+    echo "Deleting branch $1..."
+    git push origin --delete $1 || error_exit "Failed to delete branch $1"
+}
+
+function delete_stale_branches() {
+    create_delete_list
+
+    for BRANCH_NAME in ${DELETE_BRANCH_LIST[@]}
+    do
+        delete_branch $BRANCH_NAME
+    done
+}
+
+function create_delete_list() {
+    for branch in ${ALL_BRANCH_ARRAY[@]}
+    do
+        #Ahead list includes the behind and ahead commit counts
+        AHEAD_LIST=$(git rev-list --left-right --count origin/master...$branch)
+        AHEAD_ARRAY=($AHEAD_LIST)
+        if [[ ${AHEAD_ARRAY[1]} -eq 0 && ${AHEAD_ARRAY[0]} -gt 0 ]]
+        then
+            DELETE_BRANCH_LIST+=(${branch//origin\/})
+        fi
+    done
+}
+
+init
+delete_stale_branches
